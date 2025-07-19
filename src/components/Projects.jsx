915f99cce1f481556c8e3527482eb90dc232f064
Janet Oluwanifemi <janetatfuta2017@gmail.com>
2025-07-19 15:38:47 +0100

    Checked out the images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,8 +11,354 @@ const Projects = () => {
         <div class="h-2 w-3/4 bg-gradient-to-r from-teal-400 to-purple-600 rounded-full"></div>
       </div>
       {/* cards */}
-      <div class="grid md:grid-cols-2 gap-8">
+      <div class="grid md:grid-cols-3 gap-8">
         {/* first project */}
+        <div class="max-w-md mx-auto text-white rounded-xl shadow-lg overflow-hidden border border-gray-700 bg-[#241023] transition-colors duration-500 ease-in-out">
+          {/* Project Screenshot */}
+          <a href="https://productcart-six.vercel.app/" target="_blank">
+            <img
+              src="/images/productlist.png"
+              alt="Project Screenshot"
+              class="w-full h-52 object-cover"
+            />
+          </a>
+          <div class="p-5 space-y-4">
+            <a
+              class="text-xl font-semibold text-[#47A025]"
+              href="https://productcart-six.vercel.app/"
+              target="_blank"
+            >
+              Product List with Cart
+            </a>
+            <p class="text-gray-300 text-sm">
+              A frontend shopping cart app where users can add items to their
+              cart and see the total price update in real-time. Built to
+              stimulate e-commerce experience
+            </p>
+            {/* Meta Info */}
+            <div class="flex justify-between items-center text-sm text-gray-400">
+              <span class="flex items-center gap-1">
+                <CalendarDays /> <span>Start: 11 Mar 2025</span>
+              </span>
+              <span class="flex items-center gap-1 pr-20">
+                <Cable />
+                status:
+                <span class="bg-green-600 text-white text-xs px-2 py-1 rounded">
+                  Completed
+                </span>
+              </span>
+            </div>
+            {/* Tech Stack Badges */}
+            <div class="flex flex-wrap gap-2">
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                HTML
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                CSS
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                Javascript
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                React.Js
+              </span>
+            </div>
+            {/* Buttons  */}
+            <div class="flex gap-4 pt-3">
+              <a
+                href="https://github.com/Janetngozi/productcart"
+                target="_blank"
+                class="flex-1 text-center bg-gray-800 hover:bg-gray-700 py-2 px-4 rounded text-sm transition"
+              >
+                View Code
+              </a>
+              <a
+                href="https://productcart-six.vercel.app/"
+                target="_blank"
+                class="flex-1 text-center bg-[#47A025] hover:opacity-90 py-2 px-4 rounded text-sm transition"
+              >
+                Live Demo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="max-w-md mx-auto text-white rounded-xl shadow-lg overflow-hidden border border-gray-700 bg-[#241023] transition-colors duration-500 ease-in-out">
+          {/* Project Screenshot */}
+          <a href="https://food-wagon-psi.vercel.app/" target="_blank">
+            <img
+              src="/images/foodwagon.png"
+              alt="Project Screenshot"
+              class="w-full h-52 object-cover"
+            />
+          </a>
+          <div class="p-5 space-y-4">
+            <a
+              class="text-xl font-semibold text-[#47A025]"
+              href="https://food-wagon-psi.vercel.app/"
+              target="_blank"
+            >
+              Food Wagon
+            </a>
+            <p class="text-gray-300 text-sm">
+              A user-focused food delivery frontend application where users can
+              explore different dishes, view food details, and simulate the
+              ordering process—built to reflect the structure of a real-world
+              food delivery platform.
+            </p>
+            {/* Meta Info */}
+            <div class="flex justify-between items-center text-sm text-gray-400">
+              <span class="flex items-center gap-1">
+                <CalendarDays /> <span>Start: 11 Mar 2025</span>
+              </span>
+              <span class="flex items-center gap-1 pr-20">
+                <Cable />
+                status:
+                <span class="bg-green-600 text-white text-xs px-2 py-1 rounded">
+                  Completed
+                </span>
+              </span>
+            </div>
+            {/* Tech Stack Badges */}
+            <div class="flex flex-wrap gap-2">
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                TailwindCSS
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                HTML
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                Javascript
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                React.Js
+              </span>
+            </div>
+            {/* Buttons  */}
+            <div class="flex gap-4 pt-3">
+              <a
+                href="https://github.com/Janetngozi/Food-wagon"
+                target="_blank"
+                class="flex-1 text-center bg-gray-800 hover:bg-gray-700 py-2 px-4 rounded text-sm transition"
+              >
+                View Code
+              </a>
+              <a
+                href="https://food-wagon-psi.vercel.app/"
+                target="_blank"
+                class="flex-1 text-center bg-[#47A025] hover:opacity-90 py-2 px-4 rounded text-sm transition"
+              >
+                Live Demo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="max-w-md mx-auto text-white rounded-xl shadow-lg overflow-hidden border border-gray-700 bg-[#241023] transition-colors duration-500 ease-in-out">
+          {/* Project Screenshot */}
+          <a href="https://productcart-six.vercel.app/" target="_blank">
+            <img
+              src="/images/productlist.png"
+              alt="Project Screenshot"
+              class="w-full h-52 object-cover"
+            />
+          </a>
+          <div class="p-5 space-y-4">
+            <a
+              class="text-xl font-semibold text-[#47A025]"
+              href="https://productcart-six.vercel.app/"
+              target="_blank"
+            >
+              Product List with Cart
+            </a>
+            <p class="text-gray-300 text-sm">
+              A frontend shopping cart app where users can add items to their
+              cart and see the total price update in real-time. Built to
+              stimulate e-commerce experience
+            </p>
+            {/* Meta Info */}
+            <div class="flex justify-between items-center text-sm text-gray-400">
+              <span class="flex items-center gap-1">
+                <CalendarDays /> <span>Start: 11 Mar 2025</span>
+              </span>
+              <span class="flex items-center gap-1 pr-20">
+                <Cable />
+                status:
+                <span class="bg-green-600 text-white text-xs px-2 py-1 rounded">
+                  Completed
+                </span>
+              </span>
+            </div>
+            {/* Tech Stack Badges */}
+            <div class="flex flex-wrap gap-2">
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                HTML
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                CSS
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                Javascript
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                React.Js
+              </span>
+            </div>
+            {/* Buttons  */}
+            <div class="flex gap-4 pt-3">
+              <a
+                href="https://github.com/Janetngozi/productcart"
+                target="_blank"
+                class="flex-1 text-center bg-gray-800 hover:bg-gray-700 py-2 px-4 rounded text-sm transition"
+              >
+                View Code
+              </a>
+              <a
+                href="https://productcart-six.vercel.app/"
+                target="_blank"
+                class="flex-1 text-center bg-[#47A025] hover:opacity-90 py-2 px-4 rounded text-sm transition"
+              >
+                Live Demo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="max-w-md mx-auto text-white rounded-xl shadow-lg overflow-hidden border border-gray-700 bg-[#241023] transition-colors duration-500 ease-in-out">
+          {/* Project Screenshot */}
+          <a href="https://productcart-six.vercel.app/" target="_blank">
+            <img
+              src="/images/productlist.png"
+              alt="Project Screenshot"
+              class="w-full h-52 object-cover"
+            />
+          </a>
+          <div class="p-5 space-y-4">
+            <a
+              class="text-xl font-semibold text-[#47A025]"
+              href="https://productcart-six.vercel.app/"
+              target="_blank"
+            >
+              Product List with Cart
+            </a>
+            <p class="text-gray-300 text-sm">
+              A frontend shopping cart app where users can add items to their
+              cart and see the total price update in real-time. Built to
+              stimulate e-commerce experience
+            </p>
+            {/* Meta Info */}
+            <div class="flex justify-between items-center text-sm text-gray-400">
+              <span class="flex items-center gap-1">
+                <CalendarDays /> <span>Start: 11 Mar 2025</span>
+              </span>
+              <span class="flex items-center gap-1 pr-20">
+                <Cable />
+                status:
+                <span class="bg-green-600 text-white text-xs px-2 py-1 rounded">
+                  Completed
+                </span>
+              </span>
+            </div>
+            {/* Tech Stack Badges */}
+            <div class="flex flex-wrap gap-2">
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                HTML
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                CSS
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                Javascript
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                React.Js
+              </span>
+            </div>
+            {/* Buttons  */}
+            <div class="flex gap-4 pt-3">
+              <a
+                href="https://github.com/Janetngozi/productcart"
+                target="_blank"
+                class="flex-1 text-center bg-gray-800 hover:bg-gray-700 py-2 px-4 rounded text-sm transition"
+              >
+                View Code
+              </a>
+              <a
+                href="https://productcart-six.vercel.app/"
+                target="_blank"
+                class="flex-1 text-center bg-[#47A025] hover:opacity-90 py-2 px-4 rounded text-sm transition"
+              >
+                Live Demo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="max-w-md mx-auto text-white rounded-xl shadow-lg overflow-hidden border border-gray-700 bg-[#241023] transition-colors duration-500 ease-in-out">
+          {/* Project Screenshot */}
+          <a href="https://productcart-six.vercel.app/" target="_blank">
+            <img
+              src="/images/productlist.png"
+              alt="Project Screenshot"
+              class="w-full h-52 object-cover"
+            />
+          </a>
+          <div class="p-5 space-y-4">
+            <a
+              class="text-xl font-semibold text-[#47A025]"
+              href="https://productcart-six.vercel.app/"
+              target="_blank"
+            >
+              Product List with Cart
+            </a>
+            <p class="text-gray-300 text-sm">
+              A frontend shopping cart app where users can add items to their
+              cart and see the total price update in real-time. Built to
+              stimulate e-commerce experience
+            </p>
+            {/* Meta Info */}
+            <div class="flex justify-between items-center text-sm text-gray-400">
+              <span class="flex items-center gap-1">
+                <CalendarDays /> <span>Start: 11 Mar 2025</span>
+              </span>
+              <span class="flex items-center gap-1 pr-20">
+                <Cable />
+                status:
+                <span class="bg-green-600 text-white text-xs px-2 py-1 rounded">
+                  Completed
+                </span>
+              </span>
+            </div>
+            {/* Tech Stack Badges */}
+            <div class="flex flex-wrap gap-2">
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                HTML
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                CSS
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                Javascript
+              </span>
+              <span class="bg-gray-800 text-xs px-2 py-1 rounded cursor-pointer">
+                React.Js
+              </span>
+            </div>
+            {/* Buttons  */}
+            <div class="flex gap-4 pt-3">
+              <a
+                href="https://github.com/Janetngozi/productcart"
+                target="_blank"
+                class="flex-1 text-center bg-gray-800 hover:bg-gray-700 py-2 px-4 rounded text-sm transition"
+              >
+                View Code
+              </a>
+              <a
+                href="https://productcart-six.vercel.app/"
+                target="_blank"
+                class="flex-1 text-center bg-[#47A025] hover:opacity-90 py-2 px-4 rounded text-sm transition"
+              >
+                Live Demo
+              </a>
+            </div>
+          </div>
+        </div>
         <div class="max-w-md mx-auto text-white rounded-xl shadow-lg overflow-hidden border border-gray-700 bg-[#241023] transition-colors duration-500 ease-in-out">
           {/* Project Screenshot */}
           <a href="https://productcart-six.vercel.app/" target="_blank">
