e0795349a1e6a13bcdb5f20962bfbf57be7067bd
Janet Oluwanifemi <janetatfuta2017@gmail.com>
2026-01-08 15:22:49 +0100

    removed the icon at the browser

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,94 +25,7 @@ const Navbar = () => {
         </div>
 
 
-        {/* <div className="flex  cursor-pointer">
-          <a
-            href="#home"
-            className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2"
-          >
-            Home
-          </a>
-          <a
-            href="#about"
-            className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2"
-          >
-            About
-          </a>
-          <a
-            href="#goals"
-            className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2"
-          >
-            Goals
-          </a>
-          <a
-            href="#experience"
-            className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2"
-          >
-            Experience
-          </a>
-          <a
-            href="#projects"
-            className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2"
-          >
-            Projects
-          </a>
-          <a
-            href="#skills"
-            className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2"
-          >
-            Skills
-          </a>
-          <a
-            href="#testimonials"
-            className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2"
-          >
-            Testimonials
-          </a>
-          <a href="#faq" className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2">
-            FAQ
-          </a>
-          <a
-            href="#resume"
-            className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2"
-          >
-            Resume
-          </a>
-          <a
-            href="#contact"
-            className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2"
-          >
-            Contact
-          </a>
-        </div> */}
-
-        {/* Right: Nav Links */}
-        {/* <div
-          className={`${
-            menuOpen ? "flex" : "hidden"
-          } absolute top-full left-0 w-full bg-[#241023] flex-col items-start px-6 py-4 gap-2 md:gap-0 md:relative md:flex md:flex-row md:items-center md:bg-transparent md:p-0 md:top-auto md:left-auto`}
-        >
-          {[
-            "home",
-            "about",
-            "goals",
-            "experience",
-            "projects",
-            "skills",
-            "testimonials",
-            "faq",
-            "resume",
-            "contact",
-          ].map((link) => (
-            <a
-              key={link}
-              href={`#${link}`}
-              className="hover:bg-[#47A025]/50 rounded-sm px-4 py-2 capitalize w-full md:w-auto"
-              onClick={() => setMenuOpen(false)} // closes menu on link click (optional)
-            >
-              {link}
-            </a>
-          ))}
-        </div> */}
+       
 
 <div
   className={`${menuOpen ? "flex" : "hidden"} 
@@ -150,3 +63,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
